34beb06df5a6410761795154cc72afd5a0ac799c
xnn <you@example.com>
2026-05-02 20:58:56 +0800

    data fix

--- app/src/main/java/com/selves/xnn/data/entity/MemberDiaryEntity.kt
+++ app/src/main/java/com/selves/xnn/data/entity/MemberDiaryEntity.kt
@@ -1,9 +1,13 @@
 package com.selves.xnn.data.entity
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "member_diaries")
+@Entity(
+    tableName = "member_diaries",
+    indices = [Index("memberId")]
+)
 data class MemberDiaryEntity(
     @PrimaryKey
     val id: String,
